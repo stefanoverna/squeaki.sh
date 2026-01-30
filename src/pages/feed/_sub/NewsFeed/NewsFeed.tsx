47966e340d6cb1dce3cd42604043d260a8724120
Stefano Verna <s.verna@datocms.com>
2026-01-30 08:41:06 +0100

    Increase polling time

--- src/pages/feed/_sub/NewsFeed/NewsFeed.tsx
+++ src/pages/feed/_sub/NewsFeed/NewsFeed.tsx
@@ -3,7 +3,7 @@ import { keyBy } from 'lodash-es';
 import { useCallback, useEffect, useRef, useState } from 'react';
 import styles from './NewsFeed.module.css';
 
-const POLL_INTERVAL_MS = 3000;
+const POLL_INTERVAL_MS = 10000;
 
 import type { GroupedItem, Source } from '../utils';
 import { FeedItemCard } from '../FeedItemCard/FeedItemCard';
